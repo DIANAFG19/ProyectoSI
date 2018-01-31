5d1432dc6b465436f441ae3cee7a5b53a791e5de
Diana Flores García <diana_1310fg@hotmail.com>
2018-01-31 00:30:03 -0600

    Encriptado y Desencriptado en Cypher. Funcionando.

---
+++ src/encriptado/Main.java
@@ -0,0 +1,14 @@
+package encriptado;
+/*
+ * @author YOO
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        String key = "92AE31A79FEEB2A3"; //llave
+        String iv = "0123456789ABCDEF"; // vector de inicialización
+        String cleartext = "hola";
+        //System.out.println("Texto encriptado: " + encriptado.StringEncrypt.encrypt(key,));
+        //System.out.println("Texto desencriptado: " + encriptado.StringEncrypt.decrypt(key, iv, encriptado.StringEncrypt.encrypt(key, iv, cleartext)));
+    }
+}
