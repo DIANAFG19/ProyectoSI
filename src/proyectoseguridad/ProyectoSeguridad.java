80b6571a7ee45efa308ee57f6682a792dd29fef8
Diana Flores García <diana_1310fg@hotmail.com>
2018-01-31 00:16:48 -0600

    Encriptado y Desencriptado en un documento.

--- src/proyectoseguridad/ProyectoSeguridad.java
+++ src/proyectoseguridad/ProyectoSeguridad.java
@@ -1,21 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectoseguridad;
-
-/**
- *
+/*
  * @author YOO
  */
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import org.apache.commons.codec.binary.Base64;
+
 public class ProyectoSeguridad {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Base64 base64 = new Base64();
+        /*----------------ARCHIVOS------------------*/
+
+		File file = new File("C:\\Users\\YOO\\Desktop\\ProyectoSeguridad");
+		byte[] fileArray = new byte[(int) file.length()];
+		InputStream inputStream;
+
+		String encodedFile = "";
+		try {
+			inputStream = new FileInputStream(file);
+			inputStream.read(fileArray);
+			encodedFile = base64.encodeToString(fileArray);
+		} catch (Exception e) {
+			// Manejar Error
+		}
+		System.out.println(encodedFile);
+
+		/*----------------CADENAS------------------*/
+		String cadena = "Cadena";
+		String encodedString = "";
+		encodedString = base64.encodeToString(cadena.getBytes());
+		System.out.println(encodedString);
     }
     
 }
