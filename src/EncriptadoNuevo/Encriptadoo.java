57ddfbb99758208c9d8c62dab1a80b9a3dd699b3
Diana Flores García <diana_1310fg@hotmail.com>
2018-01-31 10:36:28 -0600

    Encriptado pidiendo al usuario el String que desee encriptar.

--- src/EncriptadoNuevo/Encriptadoo.java
+++ src/EncriptadoNuevo/Encriptadoo.java
@@ -4,14 +4,18 @@ package EncriptadoNuevo;
  * @author YOO
  */
 
+import java.util.Scanner;
 import org.apache.commons.codec.digest.DigestUtils;
 
 public class Encriptadoo {
 
     public static void main(String[] args) {
         
-        String textoSinEncriptar = "DIANA LAURA FLORES GARCÍA";
+        System.out.println("Escribe lo que quieras encriptar: ");
         
+        Scanner entrada = new Scanner(System.in);
+        String textoSinEncriptar = entrada.nextLine();
+
         String textoEncriptadoConMD5 = DigestUtils.md5Hex(textoSinEncriptar);
         System.out.println("Texto Encriptado con MD5 : " + textoEncriptadoConMD5);
 
--- src/encriptado/Main.java
+++ src/encriptado/Main.java
@@ -1,4 +1,7 @@
 package encriptado;
+
+import java.util.Scanner;
+
 /*
  * @author YOO
  */
@@ -7,7 +10,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         String key = "92AE31A79FEEB2A3"; //llave
         String iv = "0123456789ABCDEF"; // vector
-        String cleartext = "hola";
+        
+        System.out.println("Escribe lo que quieras encriptar: ");
+        
+        Scanner entrada = new Scanner(System.in);
+        String cleartext = entrada.nextLine();
+        
         //System.out.println("Texto encriptado: " + encriptado.StringEncrypt.encrypt(key,));
         //System.out.println("Texto desencriptado: " + encriptado.StringEncrypt.decrypt(key, iv, encriptado.StringEncrypt.encrypt(key, iv, cleartext)));
     }
